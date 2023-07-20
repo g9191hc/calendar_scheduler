aa18a6a57c1e24a4185a3839196a583ea9142016
DasSalz <g9191hc@gmail.com>
2023-07-20 16:41:25 +0900

    16.4.3

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,14 +1,45 @@
+import 'package:calendar_scheduler/component/main_calendar.dart';
+import 'package:calendar_scheduler/component/schedule_card.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime selectedDate = DateTime.utc(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("HomeScreen"),
+      body: SafeArea(
+        child: Column(
+          children: [
+            MainCalendar(
+              onDaySelected: onDaySelected,
+              selectedDate: selectedDate,
+            ),
+            ScheduleCard(
+              startTime: 12,
+              endTime: 14,
+              content: '프로그래밍 공부',
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    setState(() {
+      this.selectedDate = selectedDay;
+    });
   }
 }
